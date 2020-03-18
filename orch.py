2c1c05056a71c2112d743f58e2adc25cff725cd6
Robin <robin@naturewake.com>
2020-03-18 11:50:08 +0800

    orchestration is ready

---
+++ orch.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python3
+from lib.network_builder import create_networks
+from lib.image_builder import build_images
+from lib.container_builder import copy_to_container
+from lib.container_builder import build_containers
+from lib.db_builder import create_postgres_databases
+from engine import deploy_repository
+from engine import deploy_agent
+import time
+
+EnvLevels = ["infrastructure", "sardines", "services"]
+
+def setupEnv(
+    level:str = None,
+    skipLevel: str = None,
+    networkConfFile: str = None,
+    imageConfFile: str = None,
+    containerConfFile: str = None,
+    dbConfFileList: list = None,
+    repoDeployFileList: list = None,
+    repoHostList: list = None,
+    agentHostList: list = None
+):
+    """Only setup the environment for the future tests
+    """
+    if level is None or level not in EnvLevels:
+        raise Exception('illegal level {}'.format(level))
+
+    skipIndex = -1
+    if skipLevel is not None and skipLevel in EnvLevels:
+        skipIndex = EnvLevels.index(skipLevel)
+
+    levelIndex = EnvLevels.index(level)
+    for step in range(len(EnvLevels)):
+        if step <= skipIndex:
+            continue
+        if step > levelIndex:
+            break
+        if step == 0:
+            create_networks(networkConfFile)
+            print('networks have been created')
+            build_images(imageConfFile)
+            print('images have been built')
+            build_containers(containerConfFile)
+            for dbConfFile in dbConfFileList:
+                create_postgres_databases(dbConfFile)
+            print('test envrionment has been set at [{}] level'.format(EnvLevels[step]))
+        elif step == 1:
+            print('begin to deploy repositories')
+            for i in range(min(len(repoDeployFileList), len(repoHostList))):
+                deploy_repository(repoHostList[i], repoDeployFileList[i])
+            print('begin to deploy agents')
+            for i in range(len(agentHostList)):
+                deploy_agent(agentHostList[i], repoHostList[0])
+            print('test envrionment has been set at [{}] level'.format(EnvLevels[step]))
+            pass
+        elif step == 2:
+            pass
+        else:
+            pass
+
+
+if __name__ == '__main__':
+    import argparse
+    argParser = argparse.ArgumentParser(description='orchestrate a test')
+    argParser.add_argument(
+        '--env-level',
+        type=str,
+        required=False,
+        default='sardines',
+        help='set the test envrionment level: {}'.format(str.join(', ', EnvLevels))
+    )
+    argParser.add_argument(
+        '--skip-level',
+        type=str,
+        required=False,
+        help='skip levels below and including the specified level in the envrionment hierarchy: {}'.format(str.join(', ', EnvLevels))
+    )
+    argParser.add_argument(
+        '--config-networks',
+        type=str,
+        required=False,
+        default="./conf/networks.json",
+        help='networks configuration file'
+    )
+    argParser.add_argument(
+        '--config-images',
+        type=str,
+        required=False,
+        default="./conf/images.json",
+        help='images configuration file'
+    )
+    argParser.add_argument(
+        '--config-containers',
+        type=str,
+        required=False,
+        default="./conf/containers.json",
+        help='containers configuration file'
+    )
+    argParser.add_argument(
+        '--config-db',
+        type=str,
+        required=False,
+        default="./conf/db-postgres-test.json,./conf/db-postgres-dev.json",
+        help='database configuration files, seperated by ","'
+    )
+    argParser.add_argument(
+        '--config-repo',
+        type=str,
+        required=False,
+        default="./conf/deploy-repository-1.json",
+        help='repository deploy plan files, seperated by ","'
+    )
+    argParser.add_argument(
+        '--repo-hosts',
+        type=str,
+        required=False,
+        default="nw-test-repo-1",
+        help='repository hosts, seperated by ",", '
+    )
+    argParser.add_argument(
+        '--agent-hosts',
+        type=str,
+        required=False,
+        default="nw-test-client-1,nw-test-client-2,nw-test-client-3",
+        help='repository hosts, seperated by ",", '
+    )
+    args = argParser.parse_args()
+    setupEnv(
+        args.env_level,
+        args.skip_level,
+        args.config_networks,
+        args.config_images,
+        args.config_containers,
+        args.config_db.split(","),
+        args.config_repo.split(","),
+        args.repo_hosts.split(","),
+        args.agent_hosts.split(",")
+    )
+
