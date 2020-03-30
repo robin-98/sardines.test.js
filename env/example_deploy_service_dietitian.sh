15a130dcf43d3c4eb891bdc7b8527be3bd8d9e61
Robin <robin@naturewake.com>
2020-03-30 18:36:00 +0800

    sardine project automatic save point

--- env/example_deploy_service_dietitian.sh
+++ env/example_deploy_service_dietitian.sh
@@ -2,4 +2,4 @@
 
 ./env/sardines.py --action exec-cmd --cmd "lib/manager/manageRepository.js --create-account=dietitian-mobile:Dietitian@2019 deploy-repository.json" --ignoreCmdErr True --hosts nw-test-repo-1
 
-./env/sardines.py --action exec-cmd --cmd "bin/deploy_service.py --repo-deploy-plan deploy-repository.json --hosts nw-test-client-1,nw-test-client-2,nw-test-client-3 --application dietitian" --ignoreCmdErr True --hosts nw-test-repo-1
+./env/sardines.py --action exec-cmd --cmd 'bin/deploy_service.py --repo-deploy-plan deploy-repository.json --hosts root@nw-test-client-1 root@nw-test-client-2 root@nw-test-client-3 --application dietitian' --env 'PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:./node_modules/.bin:./bin' --hosts nw-test-repo-1
